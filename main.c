9caef30f10f149cf6eb9c780ea0a27faafae4956
Daniel Seitenfus <daniel.seitenfus@combateafraude.com>
2024-11-11 15:19:47 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  process-management-simulator
+//
+//  Created by Daniel Seitenfus on 11/11/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
