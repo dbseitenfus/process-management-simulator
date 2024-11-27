dcc5512b9bb193c78e38cc706b1b291d6ac3cfb5
Daniel Seitenfus <daniel.seitenfus@combateafraude.com>
2024-11-27 18:56:02 -0300

    Update main.c

--- main.c
+++ main.c
@@ -7,6 +7,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "fila.h"
+#include "process.h"
+#include "device.h"
 
 void init(void);
 
@@ -15,7 +18,7 @@ int main(int argc, const char * argv[]) {
     return 0;
 }
 
-FILE* open_file(const char *filename) {
+FILE* openFile(const char *filename) {
     FILE *fptr = fopen(filename, "r");
     
     if(fptr == NULL) {
@@ -27,11 +30,46 @@ FILE* open_file(const char *filename) {
 }
 
 void init(void) {
-    FILE *file = open_file("input.txt");
+    FILE *file = openFile("input.txt");
     char line[100];
-    fgets(line, sizeof(line), file) ;
+    fgets(line, sizeof(line), file);
     int nProc = line[0] - '0';
     int nDisp = line[2] - '0';
     printf("nProc: %d\n", nProc);
     printf("nDisp: %d\n", nDisp);
+
+
+    struct Device devices[nDisp];
+    int deviceTimeIndex = 4;
+    for(int i=0; i<nDisp; i++) {
+        struct Device device;
+        device.attendanceTime = line[deviceTimeIndex] - '0';
+        deviceTimeIndex += 2;
+        devices[i] = device;
+    }
+
+    for(int i=0; i<nDisp; i++) {
+        printf("\ndevice %d: tempo %d", i, devices[i].attendanceTime);
+    }
+
+    Fila *readyQueue = cria();
+
+    int pidCounter = 0;
+    Process processes[nProc];
+    for(int i=0; i<nProc; i++) {
+        fgets(line, sizeof(line), file);
+        int tInicio = line[0] - '0';
+        Process process = createProcess(pidCounter, tInicio);
+        processes[i] = process;
+        pidCounter++;
+        insere(readyQueue, &processes[i]);
+    }
+
+    for(int i=0; i<nProc; i++) {
+        Process process = processes[i];
+        printf("\npid: %d, tInicio: %d", process.pid, process.tInicio);
+    }
+
+    exibe(readyQueue);
+
 }
