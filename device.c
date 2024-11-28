7b06e75e97f8642f59214d4da4b5b5e3e9ebe54c
Daniel Seitenfus <daniel.seitenfus@combateafraude.com>
2024-11-28 17:27:10 -0300

    Update device.c

--- device.c
+++ device.c
@@ -1,7 +1,10 @@
 #include "device.h"
+#include <stdlib.h>
 
 Device createDevice(int attendanceTime) {
     Device device;
     device.attendanceTime = attendanceTime;
     device.queue = cria();
+    device.process = NULL;
+    return device;
 }
