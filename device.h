2e68dcd8e2af9ee83f96c05395ec21d14e1db608
Daniel Seitenfus <daniel.seitenfus@combateafraude.com>
2024-11-28 17:27:08 -0300

    Update device.h

--- device.h
+++ device.h
@@ -1,8 +1,14 @@
+#ifndef  DEVICE_H
+#define  DEVICE_H
+
 #include "fila.h"
 
 typedef struct Device {
     int attendanceTime;
     Fila *queue;
+    Process *process;
 } Device;
 
 Device createDevice(int attendanceTime);
+
+#endif
