44ffb5ddf1311011aaf7988e64cd79cb2c05597a
Daniel Seitenfus <daniel.seitenfus@combateafraude.com>
2024-11-28 17:27:02 -0300

    Update process.c

--- process.c
+++ process.c
@@ -1,24 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h> 
 #include "process.h"
 
-Process createProcess(int pid, int tInicio) {
+Process createProcess(int pid, int tInicio, int programLength, int nDisp) {
     Process process;
     process.pid = pid;
     process.tInicio = tInicio; 
-    process.state = NEW_READY;
+    process.state = -1;
+    process.pc = 0;
+    process.programLength = programLength;
+    process.tDevice = 0;
+
+    process.deviceTime = (int*) malloc(nDisp * sizeof(int));
+    for(int i=0; i<nDisp; i++) {
+        process.deviceTime[i] = 0;
+    }
+    process.waitingTime = 0;
+    process.tEnd = 0;
     return process;
 }
 
 void printState(State state) {
     if (state == NEW_READY) {
-        printf("NEW_READY");
+        printf("new_ready");
     } else if (state == READY) {
-        printf("READY");
+        printf("ready");
     } else if (state == RUNNING) {
-        printf("RUNNING");
+        printf("running");
     } else if (state == BLOCKED) {
-        printf("BLOCKED");
+        printf("blocked");
     } else if (state == TERMINATED) {
-        printf("TERMINATED");
+        printf("terminated");
+    } else {
+        printf("--");
     }
 }
